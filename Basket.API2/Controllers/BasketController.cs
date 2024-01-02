5914e482ba1b82986574e98b1b5f376ef5b36fbf
Luiz Otavio <luiz_otavio71@hotmail.com>
2024-01-02 19:37:32 -0300

    iniciado repositorio , mas ja foi feito bastante config, como inj de dependencia, entidades, repositorios , controllers , adicionado o docker-compose para subir as images dos servicos etc

---
+++ Basket.API2/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using Basket.API2.Entities;
+using Basket.API2.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API2.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+         => _repository = repository ?? throw new ArgumentException(nameof(repository));
+
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
---
+++ Basket.API2/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API2.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API2.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
