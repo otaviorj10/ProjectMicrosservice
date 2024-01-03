599a1370d27858c8a6afd8c32c822115ed318954
Luiz Otavio <luiz_otavio71@hotmail.com>
2024-01-03 08:08:50 -0300

    finalizado criacao dos servicos

---
+++ Disccount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Disccount.API.Entities;
+using System.Threading.Tasks;
+
+namespace Disccount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
